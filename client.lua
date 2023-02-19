dd630f9985a8ce31308c03f7407942825298be57
ZykeWasTaken <linussnilsson03@gmail.com>
2023-02-19 23:13:58 +0100

    Added Draw3DText

--- client.lua
+++ client.lua
@@ -36,6 +36,20 @@ function Functions.DrawMissionText(text, height, length)
     DrawText(length, height)
 end
 
+function Functions.Draw3DText(coords, text, scale)
+    local onScreen,_x,_y=World3dToScreen2d(coords.x, coords.y, coords.z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+
+    SetTextScale(scale, scale)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 255)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+end
+
 function Fetch()
     return Functions
 end
