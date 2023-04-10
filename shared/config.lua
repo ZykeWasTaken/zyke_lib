298c762c14314b29cab9693a9dd212699646c291
ZykeWasTaken <linussnilsson03@gmail.com>
2023-04-10 14:59:30 +0200

    Set weapon type as item or weapon in config

--- shared/config.lua
+++ shared/config.lua
@@ -4,6 +4,7 @@ Config = Config or {}
 -- Please note that you have to disable the QBCore fetching in your framework files when switching over to ESX
 -- If you're using an up to date server, ESX Legacy when this was created, use the fetching in your fxmanifest.lua, otherwise use the fetching in the unlocked files
 Config.Framework = "QBCore" -- "QBCore" / "ESX"
+Config.WeaponTypes = "item" -- "item" / "weapon", whether to remove and give the weapon as an item or as a scrollwheel weapon, most will use item hence why it's default
 
 -- Set to true to enable debug messages, recommended to always keep true since it doesn't spam the console
 -- It only gives relevant information such as critical errors
