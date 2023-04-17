c6a5d5e05d9a432c997e771ef00d2a5f10634791
AronDeveloping <arongaming1234@gmail.com>
2023-04-17 10:56:26 +0200

    Added support for old esx and some esx_legacy 1.9.3+ exports

--- client/framework.lua
+++ client/framework.lua
@@ -1,7 +1,10 @@
 if (Config.Framework == "QBCore") then
     QBCore = exports["qb-core"]:GetCoreObject()
 elseif (Config.Fraemwork == "ESX") then
-    local ESX = exports["es_extended"]:GetSharedObject()
+    return exports.es_extended:getSharedObject()
+elseif (Config.Framework == "OLD") then
+    TriggerEvent("esx:getSharedObject", function(obj)
+        ESX = obj
 elseif (Config.Framework == "QBox") then
     QBCore = exports["qbx-core"]:GetCoreObject()
 end
