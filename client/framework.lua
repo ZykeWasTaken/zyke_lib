13335ce380240d15f1ed695fab8568889f0ec4a1
AronDeveloping <arongaming1234@gmail.com>
2023-04-17 10:51:41 +0200

    ESX Support

--- client/framework.lua
+++ client/framework.lua
@@ -1,11 +1,7 @@
 if (Config.Framework == "QBCore") then
     QBCore = exports["qb-core"]:GetCoreObject()
 elseif (Config.Fraemwork == "ESX") then
-    -- OLD WAY OF FETCHING THE FRAMEWORK IS BELOW!!!
-    -- If you're using an up to date server, ESX Legacy when this was created, use the fetching in your fxmanifest.lua
-
-    -- ESX = nil
-    -- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    local ESX = exports["es_extended"]:GetSharedObject()
 elseif (Config.Framework == "QBox") then
     QBCore = exports["qbx-core"]:GetCoreObject()
 end
