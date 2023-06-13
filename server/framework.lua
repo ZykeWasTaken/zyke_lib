7a1c860f9a9dbb20badcc460ff3dd16d65156181
ZykeWasTaken <linussnilsson03@gmail.com>
2023-06-14 01:44:06 +0200

    fix(spelling)

--- server/framework.lua
+++ server/framework.lua
@@ -1,6 +1,6 @@
 if (Config.Framework == "QBCore") then
     QBCore = exports["qb-core"]:GetCoreObject()
-elseif (Config.Fraemwork == "ESX") then
+elseif (Config.Framework == "ESX") then
     -- OLD WAY OF FETCHING THE FRAMEWORK IS BELOW!!!
     -- If you're using an up to date server, ESX Legacy when this was created, use the fetching in your fxmanifest.lua
 
