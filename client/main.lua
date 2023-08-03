1e9a8b20348e58f2c7dce3dd3d18cd09145e77d5
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2023-08-03 15:55:14 +0200

    fix(fetching ESX items)

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,8 @@
 -- On client start, fetch all ESX items as they can only be accessed on the server side for some reason
 CreateThread(function()
-    while (Framework == nil) do Wait(100) end
+    while (Framework == nil) do Wait(250) end
 
     if (Framework == "ESX") then
-        Functions.Callback("zyke_lib:GetItems", function(items)
-            ESX.Items = items
-        end)
+        ESX.Items = Functions.Callback("zyke_lib:GetItems", false)
     end
 end)
