61d17c780f2322de801c1293c7272b694c019bd2
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-05-19 22:04:42 +0200

    feat(background loop to set routingBuckets to player states)

--- server/main.lua
+++ server/main.lua
@@ -10,3 +10,16 @@ CreateThread(function()
         Wait(1000)
     end
 end)
+
+-- Keeping track of routing buckets, this is to be synced with other resources
+CreateThread(function()
+    while (true) do
+        local players = GetPlayers()
+        for i = 1, #players do
+            local bucketId = GetPlayerRoutingBucket(players[i])
+            Player(players[i]).state:set("routingBucket", bucketId, true)
+        end
+
+        Wait(500)
+    end
+end)
