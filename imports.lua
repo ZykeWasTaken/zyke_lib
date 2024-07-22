8e568427845ddb417f67b4df44d62a0d0c894397
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-07-22 04:19:50 +0200

    feat(zyke_lib warning when lib is outdated for script)

--- imports.lua
+++ imports.lua
@@ -9,6 +9,40 @@ AddEventHandler("zyke_lib:OnKeyRelease", function(id)
     HoldingKeys[id] = nil
 end)
 
+if (GetCurrentResourceName() ~= "zyke_lib") then
+    local resName = GetCurrentResourceName()
+    local libVersion = GetResourceMetadata("zyke_lib", "version", 0)
+    local scriptLibVersionRequired = GetResourceMetadata(resName, "z_version", 0)
+
+    if (scriptLibVersionRequired) then
+        if (libVersion == scriptLibVersionRequired) then goto continue end
+
+        local libVersionNums = {}
+        for value in string.gmatch(libVersion, "[^.]+") do
+            libVersionNums[#libVersionNums+1] = tonumber(value)
+        end
+
+        local scriptRequiredNums = {}
+        for value in string.gmatch(scriptLibVersionRequired, "[^.]+") do
+            scriptRequiredNums[#scriptRequiredNums+1] = tonumber(value)
+        end
+
+        for i = 1, #libVersionNums do
+            local libOutdated = libVersionNums[i] < scriptRequiredNums[i]
+
+            if (libOutdated) then
+                print("^1================================================================================")
+                print("^3" .. resName .. " requires at least v" .. scriptLibVersionRequired .. " of zyke_lib, your version is " .. libVersion .. "!")
+                print("^3This mismatch can cause the script to not work as intended, please update.")
+                print("^3You can download it here: https://github.com/ZykeWasTaken/zyke_lib/releases")
+                print("^1================================================================================^0")
+            end
+        end
+    end
+
+    ::continue::
+end
+
 Tools = {}
 Translations = {}
 Minigames = {}
