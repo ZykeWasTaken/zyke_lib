7bba1d907395aaf6f2a30accd46bb05a85945d1f
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-07-26 12:39:38 +0200

    feat(annoying update disclaimer so people don't ignore)

--- imports.lua
+++ imports.lua
@@ -33,9 +33,21 @@ if (GetCurrentResourceName() ~= "zyke_lib") then
             if (libOutdated) then
                 print("^1================================================================================")
                 print("^3" .. resName .. " requires at least v" .. scriptLibVersionRequired .. " of zyke_lib, your version is " .. libVersion .. "!")
-                print("^3This mismatch can cause the script to not work as intended, please update.")
+                print("^3This mismatch will cause the script to not work as intended, please update.")
                 print("^3You can download it here: https://github.com/ZykeWasTaken/zyke_lib/releases")
                 print("^1================================================================================^0")
+
+                CreateThread(function()
+                    Wait(1000)
+                    while (true) do
+                        print("^1Very Important!")
+                        print("^3We do not perform outside checks for script versions.")
+                        print("^3Your mismatching version of zyke_lib is due to a recent update in any zyke script.")
+                        print("^3You can download the latest and required zyke_lib here: https://github.com/ZykeWasTaken/zyke_lib/releases^0")
+
+                        Wait(3000)
+                    end
+                end)
             end
         end
     end
