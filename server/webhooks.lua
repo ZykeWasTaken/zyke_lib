f7ae8299a458f0d64580dcab881e14554006d124
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-10-09 20:37:38 +0200

    feat(additional webhooks)

--- server/webhooks.lua
+++ server/webhooks.lua
@@ -4,4 +4,38 @@ Webhooks = {
         ["ModifiedCamera"] = "",
         ["RemovedCamera"] = "",
     },
+    ["zyke_moneylaunder"] = {
+        ["CollectedMoney"] = "",
+        ["DepositedMoney"] = "",
+        ["RobbedLocation"] = "",
+        ["LaunderedMoneyForInterval"] = "",
+    },
+    ["zyke_gangphone"] = {},
+    ["zyke_garages"] = {
+        ["StoreVehicle"] = "",
+        ["TransferGarage"] = "",
+        ["TakeOutVehicle"] = "",
+        ["TransferOwnership"] = "",
+        ["InitializeVehicle"] = "",
+        ["SetNewNickname"] = "",
+        ["ManagedAccess"] = "",
+        ["SetFakePlate"] = "",
+        ["LockpickedVehicle"] = "",
+        ["PayImpound"] = "",
+
+        -- Admin
+        ["SetPlate"] = "",
+        ["ClaimVehicle"] = "",
+        ["PurchasedVehicle"] = "",
+        ["WipedVehicle"] = "",
+
+        -- Job specific
+        ["ImpoundedVehicle"] = "",
+        ["SavedVehicle"] = "",
+
+        -- Garage builder
+        ["CreatedGarage"] = "",
+        ["ModifiedGarage"] = "",
+        ["DeletedGarage"] = "",
+    }
 }
