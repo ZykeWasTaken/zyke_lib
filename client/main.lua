ceb8d54df2050dfef4e0ced03c526197b92310d0
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-10-11 18:59:19 +0200

    feat(currentVehicle statebag)

--- client/main.lua
+++ client/main.lua
@@ -6,3 +6,20 @@ CreateThread(function()
         ESX.Items = Functions.Callback("zyke_lib:GetItems", false)
     end
 end)
+
+-- Set the vehicle you are currently in as a statebag
+CreateThread(function()
+    local prevVeh = 0
+
+    while (true) do
+        local sleep = 250
+        local veh = GetVehiclePedIsIn(PlayerPedId(), false)
+
+        if (prevVeh ~= veh) then
+            LocalPlayer.state:set("currentVehicle", veh, false)
+            prevVeh = veh
+        end
+
+        Wait(sleep)
+    end
+end)
