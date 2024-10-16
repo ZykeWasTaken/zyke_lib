a37130a5c5da6b1d6a17540991ef0c235dc2ce38
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-10-16 22:26:51 +0200

    feat(proper framework fetch)

--- shared/auto_fetchers/framework.lua
+++ shared/auto_fetchers/framework.lua
@@ -1,7 +1,9 @@
+Framework = nil
+
 -- Will shuffle through top to bottom, should ensure that you find your framework automatically
 -- Is your framework named something else? Make sure to fill out all values properly
 -- Do not change the variables, keep them as either "QBCore" or "ESX", bsaed on your framework
-local frameworks = {
+local systems = {
     {fileName = "qb-core", variable = "QBCore", fetching = function(fileName)
         QBCore = exports[fileName]:GetCoreObject()
     end},
@@ -14,22 +16,17 @@ local frameworks = {
     end},
 }
 
--- Fetching your framework automatically
-CreateThread(function()
-    Framework = nil
+for _, settings in pairs(systems) do
+    local resourceState = GetResourceState(settings.fileName)
 
-    for _, settings in pairs(frameworks) do
-        local resourceState = GetResourceState(settings.fileName)
+    if (resourceState == "started") then
+        Framework = settings.variable
 
-        if (resourceState == "started") then
-            Framework = settings.variable
-
-            settings.fetching(settings.fileName)
-            break
-        end
+        settings.fetching(settings.fileName)
+        break
     end
+end
 
-    if (Framework == nil) then
-        error("Could not find your framework, this is critical!")
-    end
-end)
+if (Framework == nil) then
+    error("Could not find your framework, this is critical!")
+end
