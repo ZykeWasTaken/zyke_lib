dab6b34941b276609ee90ef914322bee3441481e
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-10-22 13:34:42 +0200

    feat(queue entity removal)

--- server/functions.lua
+++ server/functions.lua
@@ -1392,6 +1392,26 @@ RegisterNetEvent("zyke_lib:InventoryUpdated", function(action)
     end
 end)
 
+---@param entity integer @Handle
+---@param duration integer @Milliseconds
+---@param freeze boolean?
+function Functions.QueueEntityRemoval(entity, duration, freeze)
+    if (not DoesEntityExist(entity)) then return end
+
+    Entity(entity).state:set("removing", duration, true)
+
+    if (freeze == true) then
+        FreezeEntityPosition(entity, true)
+    end
+
+    CreateThread(function()
+        Wait(duration)
+
+        if (not DoesEntityExist(entity)) then return end
+        DeleteEntity(entity)
+    end)
+end
+
 CreateThread(function()
     Wait(1000)
 
