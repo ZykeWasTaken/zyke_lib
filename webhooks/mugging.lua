9e2f587aedff4075b96f3b765905212cf01f2b12
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-02 15:41:51 +0100

    chore(oopsies)

--- webhooks/mugging.lua
+++ webhooks/mugging.lua
@@ -1,5 +1,5 @@
 return {
-    ["RobberyInitiated"] = "https://discord.com/api/webhooks/1120079546503413840/ajLnE4HWX1N4ynl0URpKe97IbsELUn85PzhoKtef1lg-p5Tviobx38oEfnF0rKUVD5vO",
+    ["RobberyInitiated"] = "",
     ["RobberyFinished"] = "",
     ["PickupCreated"] = "",
     ["LootCollected"] = "",
--- webhooks/plants.lua
+++ webhooks/plants.lua
@@ -1,6 +1,6 @@
 return {
-    ["OnPlant"] = "https://discord.com/api/webhooks/1300320118898819125/8vM6jYrXv8xFQM7wLKSN7sNUKOZYcSyqRdwqU6NIX-S_squUcXugNADQP05y_JmsKMKW",
-    ["OnHandle"] = "https://discord.com/api/webhooks/1300320384154861578/ZP5WvYSNlGlksfYf74aX_wda4znx7pe9AcRK-hXZIzumiSFZIAgARmWYLAY8xOS9khX-",
-    ["OnHarvest"] = "https://discord.com/api/webhooks/1300320651583688714/BXit14pTJSih_p1mFIELkzoJoLrP06CFp0d7ecEM2STYlpynPwqtdOmFbe_2EjSnvtMq",
-    ["OnLight"] = "https://discord.com/api/webhooks/1300320832249397269/ywMwnF8vq3T9bRjIWfaoFj0k5gS5TkVBUSq-cRnZVTvN6k3rmr4cbj7gaxtlWsR8_8E-",
+    ["OnPlant"] = "",
+    ["OnHandle"] = "",
+    ["OnHarvest"] = "",
+    ["OnLight"] = "",
 }
