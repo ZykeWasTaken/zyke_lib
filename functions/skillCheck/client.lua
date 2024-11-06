3267d307972d5628c726d6356ce145066f23b4c0
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-06 10:38:50 +0100

    feat(basic skillcheck)

---
+++ functions/skillCheck/client.lua
@@ -0,0 +1,9 @@
+-- Temporarily moving skill check into here
+-- More alternatives to come in the future
+
+---@param difficulty string
+function Functions.skillCheck(difficulty)
+    return lib.skillCheck(difficulty)
+end
+
+return Functions.skillCheck
