a3528f4da4ab1576bbd1f77902c6801bc0178368
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-06 20:06:57 +0100

    fix(correctly accessing QB job)

--- functions/getJob/server.lua
+++ functions/getJob/server.lua
@@ -2,10 +2,14 @@
 ---@return PlayerJob | nil
 ---@diagnostic disable-next-line: duplicate-set-field
 function Functions.getJob(player)
-    local playerData = Functions.getPlayerData(player)
-    if (not playerData or not playerData.job) then return nil end
+    local player = Functions.getPlayerData(player)
+    if (not player) then return nil end
 
-    return Formatting.formatPlayerJob(playerData.job)
+    local job
+    if (Framework == "ESX") then job = player.job end
+    if (Framework == "QB") then job = player.PlayerData.job end
+
+    return Formatting.formatPlayerJob(job)
 end
 
 return Functions.getJob
