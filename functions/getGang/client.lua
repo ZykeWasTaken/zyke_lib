ef636fb52ed0fce27a270b0db25f4d9398286526
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-08 12:03:45 +0100

    fix(verify player gang exists)

--- functions/getGang/client.lua
+++ functions/getGang/client.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable-next-line: duplicate-set-field
 function Functions.getGang()
     local player = Functions.getPlayerData()
-    if (not player) then return nil end
+    if (not player or not player.gang) then return nil end
 
     if (Framework == "QB") then return Formatting.formatPlayerGang(player.gang) end
 
