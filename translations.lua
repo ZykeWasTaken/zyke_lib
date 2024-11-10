5ce63a412718d76724d8b640429147fa52afa2ab
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-11 00:59:30 +0100

    feat(check if locales are being used)

--- translations.lua
+++ translations.lua
@@ -26,11 +26,29 @@ function Translate(key, formatting)
     return msg, notiType
 end
 
+---@return boolean
+local function dirExists()
+    local num = GetNumResourceMetadata(ResName, "file")
+    if (num == 0) then return false end
+
+    for i = 1, num do
+        local str = GetResourceMetadata(ResName, "file", i - 1)
+
+        if (str:sub(1, 8) == "locales/") then
+            return true
+        end
+    end
+
+    return true
+end
+
 local function notFound()
     error(("Translation \"%s\" not found. (locales/%s.lua)"):format(language, language))
 end
 
 if (language == nil) then return notFound() end
+
+if (not dirExists()) then return nil end
 
 local foundTranslations = LoadResourceFile(ResName, ("locales/%s.lua"):format(language))
 if (not foundTranslations) then return notFound() end
