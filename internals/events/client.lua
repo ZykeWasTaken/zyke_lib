35cfa0ab88bb7da945e8cb9f381d9a37d9464c8c
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-22 00:07:14 +0100

    feat(native ox_inventory inventory update event)

--- internals/events/client.lua
+++ internals/events/client.lua
@@ -2,26 +2,33 @@
 
 -- Framework updates
 -- For QB there is no event dispatched, has to catch the SetPlayerData event
-if (Framework == "QB") then
-    RegisterNetEvent("QBCore:Player:SetPlayerData", function()
+if (Inventory == "OX") then
+    AddEventHandler('ox_inventory:updateInventory', function()
         TriggerEvent("zyke_lib:InventoryUpdated", "update")
         TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
     end)
-elseif (Framework == "ESX") then
-    RegisterNetEvent("esx:addInventoryItem", function()
-        TriggerEvent("zyke_lib:InventoryUpdated", "add")
-        TriggerServerEvent("zyke_lib:InventoryUpdated", "add")
-    end)
+else
+    if (Framework == "QB") then
+        RegisterNetEvent("QBCore:Player:SetPlayerData", function()
+            TriggerEvent("zyke_lib:InventoryUpdated", "update")
+            TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
+        end)
+    elseif (Framework == "ESX") then
+        RegisterNetEvent("esx:addInventoryItem", function()
+            TriggerEvent("zyke_lib:InventoryUpdated", "update")
+            TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
+        end)
 
-    RegisterNetEvent("esx:removeInventoryItem", function()
-        TriggerEvent("zyke_lib:InventoryUpdated", "remove")
-        TriggerServerEvent("zyke_lib:InventoryUpdated", "remove")
-    end)
+        RegisterNetEvent("esx:removeInventoryItem", function()
+            TriggerEvent("zyke_lib:InventoryUpdated", "update")
+            TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
+        end)
 
-    RegisterNetEvent("esx:setInventoryItem", function()
-        TriggerEvent("zyke_lib:InventoryUpdated", "update")
-        TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
-    end)
+        RegisterNetEvent("esx:setInventoryItem", function()
+            TriggerEvent("zyke_lib:InventoryUpdated", "update")
+            TriggerServerEvent("zyke_lib:InventoryUpdated", "update")
+        end)
+    end
 end
 
 -- Character Selection
