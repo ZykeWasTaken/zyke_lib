1e2ec2c4055a74dfd9c35d61a8a8b21ecde6c95b
ZykeWasTaken <linussnilsson03@gmail.com>
2024-11-22 00:06:54 +0100

    feat(better permission checking using ace directly)

--- functions/hasPermission/server.lua
+++ functions/hasPermission/server.lua
@@ -1,20 +1,12 @@
 ---@param player Character | CharacterIdentifier | PlayerId
+---@param permission string @Usually "command"
 ---@return boolean
 ---@diagnostic disable-next-line: duplicate-set-field
 function Functions.hasPermission(player, permission)
     local plyId = Functions.getPlayerId(player)
     if (not plyId) then return false end
 
-    if (Framework == "QB") then return QB.Functions.HasPermission(player, permission) end
-
-    if (Framework == "ESX") then
-        local plyPerm = ESX.GetPlayerFromId(plyId).getGroup()
-        local hasPerms = plyPerm == "superadmin" or plyPerm == permission
-
-        return hasPerms
-    end
-
-    return false
+    return IsPlayerAceAllowed(tostring(plyId), permission)
 end
 
 Z.callback.register(LibName .. ":HasPermission", function(plyId, permission)
