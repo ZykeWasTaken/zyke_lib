2f25d440bb696f814bcb818c9c2d9414372d02cb
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-11-27 09:18:30 +0100

    feat(get missing items as translated string)

---
+++ functions/getMissingItems/shared.lua
@@ -0,0 +1,40 @@
+-- Uses the normal hasItem function, but is combined with getItem to return a string of missing items
+-- This is mainly for notification purposes to easily clarify needed items for the user
+
+---@param player Character | CharacterIdentifier | PlayerId
+---@param requiredItems table
+function Functions.getMissingItems(player, requiredItems)
+    local player = Z.getPlayerData(player)
+    if (not player) then return false, "MISSING PLAYER" end
+
+    local items = Formatting.formatItemInput(requiredItems)
+
+    local itemsMissing = {}
+    for i = 1, #items do
+        local hasItem = Z.hasItem(player, items[i].name, items[i].amount)
+        if (not hasItem) then
+            itemsMissing[#itemsMissing+1] = {
+                name = items[i].name,
+                amount = items[i].amount
+            }
+        end
+    end
+
+    if (#itemsMissing > 0) then
+        local missingItemsStr = ""
+        for i = 1, #itemsMissing do
+            local item = Z.getItem(itemsMissing[i].name)
+            if (not item) then error(("MISSING ITEM %s"):format(itemsMissing[i].name)) return false end
+
+            missingItemsStr = missingItemsStr .. itemsMissing[i].amount .. "x " .. item.label .. ", "
+        end
+
+        missingItemsStr = missingItemsStr:sub(0, #missingItemsStr - 2)
+
+        return false, missingItemsStr
+    end
+
+    return true
+end
+
+return Functions.getMissingItems
