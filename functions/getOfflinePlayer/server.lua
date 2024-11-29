afb276144e484b4040a726a11a02186a5a49186e
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-11-29 03:12:49 +0100

    fix(fetch all required offline player data)

--- functions/getOfflinePlayer/server.lua
+++ functions/getOfflinePlayer/server.lua
@@ -3,13 +3,27 @@ function Functions.getOfflinePlayer(identifier)
     if (Framework == "QB") then return QB.Functions.GetOfflinePlayerByCitizenId(identifier) end
 
     if (Framework == "ESX") then
-        local row = MySQL.single.await("SELECT `accounts`, `inventory`, `loadout`, `position`, `skin`, `skin`, `status`, `metadata` FROM users WHERE identifier = ?", {
-            identifier
-        })
+        local query = [[
+            SELECT
+                `identifier`, `firstname`, `lastname`, `dateofbirth`, `sex`,
+                `accounts`, `inventory`, `loadout`, `position`, `skin`, `skin`, `status`, `metadata`
+            FROM
+                users
+            WHERE
+                identifier = ?
+            LIMIT 1
+        ]]
+
+        local row = MySQL.single.await(query, {identifier})
 
         if (not row) then return nil end
 
         return {
+            identifier = row.identifier,
+            firstname = row.firstname,
+            lastname = row.lastname,
+            dateofbirth = row.dateofbirth,
+            sex = row.sex,
             accounts = json.decode(row.accounts),
             inventory = json.decode(row.inventory),
             loadout = json.decode(row.loadout),
