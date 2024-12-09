e5e4a9b153ca0419f9873f1c6c82cb1739b54c9a
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-12-09 03:50:58 +0100

    feat(better ox_inventory support)

--- internals/items/server.lua
+++ internals/items/server.lua
@@ -1,44 +1,69 @@
 -- This is a multi-solution to ensuring certain metadata exists for select items for all inventories and frameworks
 -- If your inventory provides another solution, we encourage you to use it, to prevent needing to listen to these events & process items all the time
+---@type table<string, table>
 local ensuredMetadata = {}
 
-RegisterNetEvent("zyke_lib:InventoryUpdated", function(action)
-    if (not Functions.table.doesTableHaveEntries(ensuredMetadata)) then return end
+-- Cache a basic array to send into the item fetcher
+-- This will ensure only necessary items are included and processed
+---@type string[]
+local itemsToFetch = Functions.table.new({})
+
+-- TODO: Fix this bs for zyke_smoking cigarette packs, it seems that they are not using the ensure functionality, and just catching the event
+
+---@param changes? table @Ox only
+RegisterNetEvent("zyke_lib:InventoryUpdated", function(changes)
+    if (#itemsToFetch <= 0) then return end
+
+    -- Find the modified slots so we can exclusively manage them, to save performance
+    ---@type table<string, string> @slot, item name
+    local modifiedSlots = {}
+    if (changes ~= nil) then
+        for _, data in pairs(changes) do
+            if (data and data.slot) then
+                modifiedSlots[tostring(data.slot)] = data.name
+            end
+        end
+    end
+
+    local modifiedSlotsCount = Functions.table.count(modifiedSlots)
+
+    -- If changes are defined, but none of your slots are modified
+    -- This means that there is nothing we need to update
+    if (changes and modifiedSlotsCount == 0) then return end
 
     local plyId = source
+    local plyInv = Functions.getPlayerItems(plyId, itemsToFetch)
+    local modifySlots = modifiedSlotsCount > 0
 
-    if (action == "add" or action == "update") then
-        local plyInv = Functions.getPlayerItems(plyId)
+    for i = 1, #plyInv do
+        local item = plyInv[i]
+        local name = item.name
 
-        for i = 1, #plyInv do
-            local item = plyInv[i]
-            local name = item.name
+        if (modifySlots and not modifiedSlots[tostring(item.slot)]) then goto continue end
+        if (ensuredMetadata[name] == nil) then goto continue end
 
-            if (ensuredMetadata[name] == nil) then goto continue end
+        if (not item.metadata) then item.metadata = {} end
 
-            if (not item.metadata) then item.metadata = {} end
+        local newMetadata = {}
+        local added = 0
+        for metaKey, metaValue in pairs(ensuredMetadata[name]) do
+            local metadata = item.metadata[metaKey]
 
-            local newMetadata = {}
-            local added = 0
-            for metaKey, metaValue in pairs(ensuredMetadata[name]) do
-                local metadata = item.metadata[metaKey]
-
-                if (not metadata) then
-                    added += 1
-                end
-
-                local newValue = metadata == nil and metaValue or metadata
-
-                newMetadata[metaKey] = newValue
+            if (not metadata) then
+                added += 1
             end
 
-            if (added > 0) then
-                Functions.setItemMetadata(plyId, item.slot, newMetadata)
-                Functions.internalDebug(("Ensured missing metadata for %s."):format(name))
-            end
+            local newValue = metadata == nil and metaValue or metadata
 
-            ::continue::
+            newMetadata[metaKey] = newValue
         end
+
+        if (added > 0) then
+            Functions.setItemMetadata(plyId, item.slot, newMetadata)
+            Functions.internalDebug(("Ensured missing metadata for %s."):format(name))
+        end
+
+        ::continue::
     end
 end)
 
@@ -48,6 +73,10 @@ end)
 ---@param metadata table<string, any>
 exports("EnsureMetadata", function(item, metadata)
     -- TODO merge old data with new, if different scripts wants to apply data, not needed yet
+
+    if (not ensuredMetadata[item]) then
+        itemsToFetch[#itemsToFetch+1] = item
+    end
 
     ensuredMetadata[item] = metadata
 end)
