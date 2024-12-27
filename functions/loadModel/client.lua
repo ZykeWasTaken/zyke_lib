68bfb71c152fa9806cd8c5d70208e993fee043b8
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-12-27 15:37:25 +0100

    feat(possibility to skip error)

--- functions/loadModel/client.lua
+++ functions/loadModel/client.lua
@@ -1,8 +1,15 @@
 ---@param model string
+---@param skipError? boolean
 ---@return boolean
-function Functions.loadModel(model)
+function Functions.loadModel(model, skipError)
     local isValid = IsModelValid(model)
-    if (not isValid) then error("Tried to load invalid model: " .. model) return false end
+    if (not isValid) then
+        if (not skipError) then
+            error("Tried to load invalid model: " .. model)
+        end
+
+        return false
+    end
 
     RequestModel(model)
     while (not HasModelLoaded(model)) do Wait(0) end
