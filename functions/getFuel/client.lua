82a654c175db4c4f32db6f90f598a34a20fba201
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-12-30 21:52:30 +0100

    feat(get fuel level)

--- functions/getFuel/client.lua
+++ functions/getFuel/client.lua
@@ -2,6 +2,7 @@
 ---@return number
 function Functions.getFuel(veh)
     if (FuelSystem == "LegacyFuel") then return Functions.numbers.round(exports["LegacyFuel"]:GetFuel(veh), 1) end
+    if (FuelSystem == "OX") then return Functions.numbers.round(GetVehicleFuelLevel(veh), 1) end
 
     return Functions.numbers.round(GetVehicleFuelLevel(veh), 1)
 end
