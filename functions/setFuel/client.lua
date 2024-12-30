921208692c083cf9160abcae05c037ff72e0f77e
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-12-30 21:54:47 +0100

    feat(ox_fuel setFuel)

--- functions/setFuel/client.lua
+++ functions/setFuel/client.lua
@@ -1,9 +1,8 @@
 ---@param veh Vehicle
 ---@param amount number
 function Functions.setFuel(veh, amount)
-    if (FuelSystem == "LegacyFuel") then
-        return exports["LegacyFuel"]:SetFuel(veh, amount)
-    end
+    if (FuelSystem == "LegacyFuel") then return exports["LegacyFuel"]:SetFuel(veh, amount) end
+    if (FuelSystem == "OX") then Entity(veh).state.fuel = amount return end
 
     SetVehicleFuelLevel(veh, amount)
 end
