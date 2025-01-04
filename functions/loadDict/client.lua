d41fee71e7b90b312881895ad6a1233692fa3eb5
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-01-04 23:15:22 +0100

    feat(loadDict skipError param)

--- functions/loadDict/client.lua
+++ functions/loadDict/client.lua
@@ -1,8 +1,15 @@
 ---@param dict string
+---@param skipError? boolean
 ---@return boolean
-function Functions.loadDict(dict)
+function Functions.loadDict(dict, skipError)
     local isValid = DoesAnimDictExist(dict)
-    if (not isValid) then error("Tried to load invalid animation dictionary: " .. dict) return false end
+    if (not isValid) then
+        if (not skipError) then
+            error("Tried to load invalid animation dictionary: " .. dict)
+        end
+
+        return false
+    end
 
     RequestAnimDict(dict)
     while (not HasAnimDictLoaded(dict)) do Wait(0) end
