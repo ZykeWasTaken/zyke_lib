a2666501931ea02574cd9f3f0bafc119195e5c99
ZykeWasTaken <linussnilsson03@gmail.com>
2025-04-12 01:01:26 +0200

    feat(bundle desired items for cleaner missing notification)

--- functions/getMissingItems/server.lua
+++ functions/getMissingItems/server.lua
@@ -2,21 +2,48 @@
 -- This is mainly for notification purposes to easily clarify needed items for the user
 
 ---@param player Character | CharacterIdentifier | PlayerId
----@param requiredItems {name: string, amount: integer}[]
+---@param requiredItems {name: string, amount?: integer}[]
 ---@diagnostic disable-next-line: duplicate-set-field
 function Functions.getMissingItems(player, requiredItems)
     local player = Functions.getPlayerData(player)
     if (not player) then return false, "MISSING PLAYER" end
 
-    local items = Formatting.formatItemInput(requiredItems)
+    local formattedItems = Formatting.formatItemInput(requiredItems)
+    local items = {}
 
+    -- Iterate & combine amounts, since there may be duplicate items in the list
+    -- This way, we can provide a cleaner notification to the user
+    ---@type {[string]: integer} -- name: idx
+    local itemLookup = {}
+    for i = 1, #formattedItems do
+        local item = formattedItems[i]
+        local name = item.name
+
+        if (not itemLookup[name]) then
+            local idx = #items + 1
+
+            itemLookup[name] = idx
+            items[idx] = {
+                name = name,
+                amount = item.amount or 1
+            }
+        else
+            local idx = itemLookup[name]
+
+            items[idx].amount = items[idx].amount + (item.amount or 1)
+        end
+    end
+
+    ---@type {name: string, amount: integer}[]
     local itemsMissing = {}
     for i = 1, #items do
-        local hasItem = Functions.hasItem(player, items[i].name, items[i].amount)
-        if (not hasItem) then
+        local item = Functions.getPlayerItem(player, items[i].name, false, true, nil)
+        local remainingAmount = items[i].amount - ((item and item[1] and item[1].amount) or 0)
+
+        if (remainingAmount > 0) then
             itemsMissing[#itemsMissing+1] = {
                 name = items[i].name,
-                amount = items[i].amount
+                amount = remainingAmount
             }
         end
     end
