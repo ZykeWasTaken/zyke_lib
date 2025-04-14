4bfecad50891913f89de810641cd95a8211d8d28
ZykeWasTaken <linussnilsson03@gmail.com>
2025-04-14 16:48:28 +0200

    chore(I swear I removed these)

--- webhooks/consumables.lua
+++ webhooks/consumables.lua
@@ -1,25 +1,25 @@
 return {
-	["ItemAdded"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["IngredientAdded"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi", -- You may want to disable this since it might have a lot of unnecessary requests
+	["ItemAdded"] = "",
+	["IngredientAdded"] = "", -- You may want to disable this since it might have a lot of unnecessary requests
 
 	-- Item Creator
 	-- These contains little to no rawData since the request is too large to be allowed
-	["ItemCreated"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["ItemModified"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["ItemDeleted"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
+	["ItemCreated"] = "",
+	["ItemModified"] = "",
+	["ItemDeleted"] = "",
 
 	-- Ingredient Creator
 	-- These contains little to no rawData since the request is too large to be allowed
-	["IngredientCreated"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["IngredientModified"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["IngredientDeleted"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
+	["IngredientCreated"] = "",
+	["IngredientModified"] = "",
+	["IngredientDeleted"] = "",
 
 	-- These can be spammed quite a lot of you have a lot of players
-	["ItemUseStart"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["ItemUseStop"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
+	["ItemUseStart"] = "",
+	["ItemUseStop"] = "",
 
 	-- Interactions
-	["ItemPlaced"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["ItemPickup"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
-	["ItemTransfer"] = "https://discord.com/api/webhooks/1326567141025120318/YF7nw9XR7-PeWIZ9MQsZrVZdRJNkwCVsHXV05aYlaBdKmBJWZ6YOM-L5a2d6FMwu7lMi",
+	["ItemPlaced"] = "",
+	["ItemPickup"] = "",
+	["ItemTransfer"] = "",
 }
