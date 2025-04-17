ddb950146029ebd1e1e152b285cd52067fc038fe
ZykeWasTaken <linussnilsson03@gmail.com>
2025-04-17 21:31:14 +0200

    feat(extra validation)

--- functions/getClosestPlayerToPos/server.lua
+++ functions/getClosestPlayerToPos/server.lua
@@ -1,6 +1,7 @@
 ---@param pos vector3 | table{x: number, y: number, z: number}
+---@param validationFn? fun(playerId: PlayerId, ped: Ped, dst: number): boolean
 ---@return PlayerId | -1, number | -1, boolean
-function Functions.getClosestPlayerToPos(pos)
+function Functions.getClosestPlayerToPos(pos, validationFn)
     ---@type integer[]
     local players = Functions.getPlayers()
     local closestPly, closestDst = -1, -1
@@ -12,7 +13,11 @@ function Functions.getClosestPlayerToPos(pos)
         local pedPos = GetEntityCoords(ped)
         local dst = #(pedPos - pos)
 
-        if (closestDst == -1 or closestDst > dst) then
+        if (
+            (closestDst == -1 or closestDst > dst)
+            ---@diagnostic disable-next-line: need-check-nil
+            and (not validationFn and true or validationFn(players[i], ped, dst))
+        ) then
             closestPly = players[i]
             closestDst = dst
         end
