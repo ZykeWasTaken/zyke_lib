73b6d9941fe0a46c8a6fd40915b634ef8581acbb
ZykeWasTaken <linussnilsson03@gmail.com>
2025-07-16 22:13:35 +0200

    feat(use loader)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,7 @@ game "gta5"
 lua54 "yes"
 version "2.2.2"
 
-shared_scripts {
-    "@ox_lib/init.lua", -- Progressbar & skillcheck
-    "config.lua",
-    "imports.lua",
-    "internals/internals.lua",
-}
+shared_script "imports.lua"
 
 ui_page "javascript/index.html"
 
@@ -19,24 +14,33 @@ files {
     "functions/**/shared.lua",
     "translations.lua",
     "systems/*.lua",
+    "internals/**/*.lua",
     "loader.lua",
 
     "javascript/index.html",
     "javascript/*.js",
 }
 
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
+loader {
+    "@ox_lib/init.lua", -- Progressbar & skillcheck
+    "shared:config.lua",
+    "shared:internals/internals.lua",
+
+    "server:@oxmysql/lib/MySQL.lua",
     "functions/callback/server.lua",
-    "internals/**/server.lua",
+    "internals/events/server.lua",
+    "internals/items/server.lua",
+    "internals/playerIdentifiers/server.lua",
+    "internals/states/server.lua",
 
     "functions/debug/shared.lua",
-}
 
-client_scripts {
     "functions/callback/client.lua",
     "functions/debug/shared.lua",
-    "internals/**/client.lua"
+    "internals/copy/client.lua",
+    "internals/events/client.lua",
+    "internals/items/client.lua",
+    "internals/states/client.lua",
 }
 
 dependencies {
