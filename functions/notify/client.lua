159f053fc730d22f5ac84c4b2c37aab0f04548ae
ZykeWasTaken <linussnilsson03@gmail.com>
2025-12-09 14:48:52 +0100

    feat(raw notify msg)

--- functions/notify/client.lua
+++ functions/notify/client.lua
@@ -1,9 +1,16 @@
 ---@param key string
 ---@param formatting any[]
 ---@param length number? @ms
+---@param raw boolean? @ If true, we will not translate the key & use it as the translated string, it also does not apply any formatting
+---@param _notifyType string? @ If provided, we will use it as the notification type instead of the default, primarily meant to substitute for raw
 ---@diagnostic disable-next-line: duplicate-set-field
-function Functions.notify(key, formatting, length)
-    local notifyStr, notifyType = T(key, formatting)
+function Functions.notify(key, formatting, length, raw, _notifyType)
+    local notifyStr, notifyType = nil, _notifyType or "primary"
+    if (raw == true) then
+        notifyStr = key
+    else
+        notifyStr, notifyType = T(key, formatting)
+    end
 
     if (Framework == "QB") then QB.Functions.Notify(notifyStr, notifyType, length) return end
 
@@ -16,8 +23,8 @@ function Functions.notify(key, formatting, length)
     end
 end
 
-RegisterNetEvent(ResName .. ":notify", function(key, formatting, length)
-    Functions.notify(key, formatting, length)
+RegisterNetEvent(ResName .. ":notify", function(key, formatting, length, raw, _notifyType)
+    Functions.notify(key, formatting, length, raw, _notifyType)
 end)
 
 return Functions.notify
--- functions/notify/server.lua
+++ functions/notify/server.lua
@@ -2,9 +2,11 @@
 ---@param key string
 ---@param formatting any[]
 ---@param length number? @ms
+---@param raw boolean? @ If true, we will not translate the key & use it as the translated string, it also does not apply any formatting
+---@param _notifyType string? @ If provided, we will use it as the notification type instead of the default, primarily meant to substitute for raw
 ---@diagnostic disable-next-line: duplicate-set-field
-function Functions.notify(plyId, key, formatting, length)
-    TriggerClientEvent(ResName .. ":notify", plyId, key, formatting, length)
+function Functions.notify(plyId, key, formatting, length, raw, _notifyType)
+    TriggerClientEvent(ResName .. ":notify", plyId, key, formatting, length, raw, _notifyType)
 end
 
 return Functions.notify
