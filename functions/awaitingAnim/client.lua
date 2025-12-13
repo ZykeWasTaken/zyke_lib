3c66ffcf0cb578e8f821794d06bab2708e979652
ZykeWasTaken <linussnilsson03@gmail.com>
2025-12-13 16:43:43 +0100

    feat(return if playing anim)

--- functions/awaitingAnim/client.lua
+++ functions/awaitingAnim/client.lua
@@ -2,15 +2,18 @@
 ---@param dict string
 ---@param clip string
 ---@param timeout? number
+---@return boolean @ Whether the animation played successfully
 function Functions.awaitingAnim(entity, dict, clip, timeout)
-	Functions.loadDict(dict)
+	if (not Functions.loadDict(dict)) then return false end
 
 	local started = GetGameTimer()
     while (not IsEntityPlayingAnim(entity, dict, clip, 3)) do
         Wait(0)
 
-        if (timeout and GetGameTimer() - started > timeout) then return end
+        if (timeout and GetGameTimer() - started > timeout) then return false end
     end
+
+    return true
 end
 
 return Functions.awaitingAnim
