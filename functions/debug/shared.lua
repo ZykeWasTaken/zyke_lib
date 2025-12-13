2cfc04216c56bd3f43cc15bdb3a1be1b02e6c75b
ZykeWasTaken <linussnilsson03@gmail.com>
2025-12-13 16:43:15 +0100

    feat(include src path & line numbers)

--- functions/debug/shared.lua
+++ functions/debug/shared.lua
@@ -1,13 +1,38 @@
----@param ... string
-local function debug(...)
-    if (not Config.Settings.debug == true) then return end
+local luaDebug = debug
 
+--[[
+    ^1 - Red
+    ^2 - Green
+    ^3 - Yellow
+    ^4 - Blue
+    ^5 - Cyan
+    ^6 - Magenta
+    ^7 - Light Gray
+    ^8 - Dark Red
+    ^9 - Dark Blue
+    ^0 - White
+]]
+
+---@param ... string
+local function formatDebug(...)
     local str = ""
     for _, arg in pairs({...}) do
         str = str .. tostring(arg) .. " "
     end
 
-    print("^4[Debug]: ^7" .. str:sub(1, #str - 1))
+    local stackSkip = 4 -- Skips the stack levels, shows up as the zyke_lib method we're importing unless adjusted
+    local info = luaDebug.getinfo(stackSkip, "Sl")
+    local src = info.short_src -- Source path, something like @zyke_status/server/register_statuses.lua
+    local line = info.currentline
+
+    return "^4[Debug]: ^0" .. str:sub(1, #str - 1) .. (" ^6(%s:%d)^0"):format(src, line)
+end
+
+---@param ... string
+local function debug(...)
+    if (not Config.Settings.debug == true) then return end
+
+    print(formatDebug(...))
 end
 
 Functions.debug = setmetatable({}, {
@@ -20,12 +45,7 @@ Functions.debug = setmetatable({}, {
 function Functions.debug.internal(...)
     if (not LibConfig.debug == true) then return end
 
-    local str = ""
-    for _, arg in pairs({...}) do
-        str = str .. tostring(arg) .. " "
-    end
-
-    print("^4[Debug]: ^7" .. str:sub(1, #str - 1))
+    print(formatDebug(...))
 end
 
 return Functions.debug
